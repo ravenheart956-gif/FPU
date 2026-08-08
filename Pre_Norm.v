a2accc51db1596e0e3194699658b9cb70758f10e
Pavin <your-github-email@example.com>
2026-08-09 00:41:32 +0530

    Workflow of Pre-Normalisation Added

---
+++ Pre_Norm.v
@@ -0,0 +1,16 @@
+module Pre_Norm(
+    input [31:0] in1,
+    input [31:0] in2,
+);
+
+reg [31:0] in1_reg;
+reg [31:0] in2_reg;
+
+// Step 1 : Comapare the Exponents of both inputs
+
+
+// Step 2 : Alter the Numbers so that the exponents are equal
+
+// This kind of normalisation will only work for Addition and Subtraction , When It comes to division and multiplication the exponents are also changing
+
+endmodule
