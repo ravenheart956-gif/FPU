ea6d51f716369e5cdbc56f9ff35f49941a8bc2af
ravenheart956-gif <ravenheart956@gmail.com>
2026-08-08 14:32:49 +0530

    Modify SubNDet module to include mantissa output
    Added a mantissa output and removed the zero flag from the SubNDet module.

--- SubNDet.v
+++ SubNDet.v
@@ -4,12 +4,15 @@ module SubNDet(
     output normal,
     output Inf,
     output NaN,
-    output zero
+    output [23:0] man
 );
 
 assign subnormal = (in[30:23] == 8'd0 && !(in[22:0] == 23'b0));
-assign zero = (in[30:23] == 8'd0 && in[22:0] == 23'b0);
-assign normal = (!(in[30:23] == 8'd0) && !(in[22:0] == 23'b0));
+assign normal = (!(in[30:23] == 8'd0) && !(in[22:0] == 23'b0)); /*might not need normal flag*/ /*also removed the zero flag*/
+
+/*EXCEPTIONS*/
 assign Inf = (in[30:23] == 8'd255 && (in[22:0] == 23'b0));
 assign NaN = ((in[30:23] == 8'd255) && !(in[22:0] == 23'b0));
+
+assign man = subnormal ? {1'b0,in[22:0]} : {1'b1,in[22:0]};
 endmodule
