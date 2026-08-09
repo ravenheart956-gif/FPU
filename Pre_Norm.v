c1ce56fe1edccc197b1fcc4e06d2674e08b5c149
Pavin <your-github-email@example.com>
2026-08-09 21:47:38 +0530

    Added Registers to the Pre_Norm Unit

--- Pre_Norm.v
+++ Pre_Norm.v
@@ -1,13 +1,27 @@
 module Pre_Norm(
+    input clk,rst,
     input [31:0] in1,
     input [31:0] in2,
 );
 
 reg [31:0] in1_reg;
 reg [31:0] in2_reg;
+reg [7:0] del_E;
 
-// Step 1 : Comapare the Exponents of both inputs
+assign del_E = in1_reg[30:23] - in2_reg[30:23];
 
+always @(posedge clk) begin
+    if (rst)begin
+        in2_reg <= 32'd0;
+        in1_reg <= 32'd0;
+        del_E <= 8'd0;
+    end
+    else begin
+        // The numbers are then arranged in such a way that the exponents are the same, These numbers are then givin as output . This simplifies the next step, Addition and Substraction
+    end
+end
+
+endmodule
 
 // Step 2 : Alter the Numbers so that the exponents are equal
 
