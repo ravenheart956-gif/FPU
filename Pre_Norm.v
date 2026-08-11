873b806746f1ecd715ad6a7110a7aeaf7655ad50
Pavin <your-github-email@example.com>
2026-08-11 19:36:11 +0530

    Added MUXs. Work in Progress

--- Pre_Norm.v
+++ Pre_Norm.v
@@ -6,9 +6,10 @@ module Pre_Norm(
 
 reg [31:0] in1_reg;
 reg [31:0] in2_reg;
-reg [7:0] del_E;
+wire [7:0] del_E;
+wire grt;
 
-assign del_E = in1_reg[30:23] - in2_reg[30:23];
+
 
 always @(posedge clk) begin
     if (rst)begin
@@ -17,14 +18,16 @@ always @(posedge clk) begin
         del_E <= 8'd0;
     end
     else begin
-        // The numbers are then arranged in such a way that the exponents are the same, These numbers are then givin as output . This simplifies the next step, Addition and Substraction
+        in1_reg <= in1;
+        in2_reg <= in2;
     end
 end
 
+
+assign grt = (in1_reg > in2_reg);
+assign del_E = grt?(in1_reg[30:23]-in2_reg[30:23]):(in2_reg[30:23]-in1_reg[30:23]);
+
+
+
 endmodule
 
-// Step 2 : Alter the Numbers so that the exponents are equal
-
-// This kind of normalisation will only work for Addition and Subtraction , When It comes to division and multiplication the exponents are also changing
-
-endmodule
