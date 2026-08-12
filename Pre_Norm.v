86244cc01a126e4b01f8b2bf78a44cf05adb4129
Pavin <your-github-email@example.com>
2026-08-12 23:20:50 +0530

    Scratched and restarted with Pre_Norm

--- Pre_Norm.v
+++ Pre_Norm.v
@@ -2,30 +2,21 @@ module Pre_Norm(
     input clk,rst,
     input [31:0] in1,
     input [31:0] in2,
+    output reg [7:0] diff
 );
 
-reg [31:0] in1_reg;
-reg [31:0] in2_reg;
-wire [7:0] del_E;
-wire grt;
+reg [7:0] diff_reg;
 
+assign diff_reg = in1 + in2 + 1'b1;
 
-
-always @(posedge clk) begin
-    if (rst)begin
-        in2_reg <= 32'd0;
-        in1_reg <= 32'd0;
-        del_E <= 8'd0;
+always @(posedge clk ) begin
+    if(rst)begin
+        diff <= 8'b0;
     end
     else begin
-        in1_reg <= in1;
-        in2_reg <= in2;
+        diff <= diff_reg;
     end
-end
-
-
-assign grt = (in1_reg > in2_reg);
-assign del_E = grt?(in1_reg[30:23]-in2_reg[30:23]):(in2_reg[30:23]-in1_reg[30:23]);
+end 
 
 
 
